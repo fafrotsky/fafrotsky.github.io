efac9d08f72e1f7502946d4a0bac95058dfce1f5
fafrotsky <176275799+fafrotsky@users.noreply.github.com>
2025-04-27 21:03:00 -0500

    Adjust Reference Resizing

--- assets/js/rivereference.js
+++ assets/js/rivereference.js
@@ -9,3 +9,20 @@
         r.resizeDrawingSurfaceToCanvas();
       },
   });
+
+  
+function handleResize() {
+  r.resizeDrawingSurfaceToCanvas();
+}
+
+// Subscribe to window size changes
+window.addEventListener('resize', handleResize);
+
+// Subscribe to devicePixelRatio changes
+window.matchMedia(`(resolution: ${window.devicePixelRatio}dppx)`)
+  .addEventListener("change", handleResize);
+
+
+  
+  // Add event listener and provide callback to handle Rive Event
+  r.on(rive.EventType.RiveEvent, onRiveEventReceived);
